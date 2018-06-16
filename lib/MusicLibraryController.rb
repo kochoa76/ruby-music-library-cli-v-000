a1c4f41b06a7e5695e71cb26bb49ff92719a3db8
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-16 22:10:51 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -50,7 +50,7 @@ class MusicLibraryController
       end
 
       def list_genres
-        Genres.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+        Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
           puts "#{i}. #{g.name}"
         end
       end
