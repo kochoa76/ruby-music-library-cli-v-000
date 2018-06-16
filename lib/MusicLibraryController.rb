dcd4a53355954803f87c3a5defee24ce32ec5445
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-16 22:19:54 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -61,8 +61,8 @@ class MusicLibraryController
         song_input = Song.find_by_name(input)
         song_input.sort{|a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
           puts "#{i}. #{s.name} - #{s.genre.name}"
-        end
         song_input 
+      end 
       end
 
 
