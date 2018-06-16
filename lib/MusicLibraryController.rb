a5ab91d17d09ae5d9a0c20ac67ef71cffa73678b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-16 22:03:26 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -42,11 +42,11 @@ class MusicLibraryController
           puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
       end
     end
-    
+
       def list_artists
         Artists.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
           puts "#{i}. #{a.name} - #{a.song.name} - #{a.genre.name}"
-        end 
+        end
       end 
 
 
