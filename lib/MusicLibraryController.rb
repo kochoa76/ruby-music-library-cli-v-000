2dc83ef097134d1c75b42b111130cd842978b28c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-16 22:29:53 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
         input = gets.strip
 
         if genre = Genre.find_by_name(input)
-          genre.songs.{|a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+          genre.songs.{|a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
               puts "#{i}. #{s.name} - #{s.genre.name}"
           end
         end
